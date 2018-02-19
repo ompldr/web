e53dcce63226541ffbbb9c6533e7bd8ccbab5a9b
Brenden Matthews <brenden@diddyinc.com>
2018-02-19 18:23:40 -0500

    Add missing file, update gitignore.

---
+++ src/containers/App/Filedrop.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import FileUploadProgress  from 'react-fileupload-progress';
+import * as Dropzone2 from "react-dropzone";
+const Dropzone: any = Dropzone2;
+
+export namespace File {
+  export interface State {
+    files: Array<File>;
+  }
+
+  export interface Props {
+  }
+}
+
+export class Filedrop extends React.Component<File.Props, File.State> {
+  public state: File.State = {
+    files: [],
+  };
+
+  constructor(props?: File.Props, context?: File.State) {
+    super(props, context);
+    this.state = { files: [] };
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
+  render() {
+    const { files } = this.state;
+    return (
+      <section>
+      <div>
+        <Dropzone onDrop={this.onDrop.bind(this)} >
+        <p>Drop yo files here!</p>
+        </Dropzone>
+        </div>
+      <aside>
+      <h2>Dropped files</h2>
+      <ul>
+       { files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>) }
+      </ul>
+      </aside>
+      </section>
+    );
+}
+}
